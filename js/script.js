401f999012e1e3b2a5df78f50b3fb8fc7af92d22
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-02-03 17:27:03 +0100

    dwadhiuwahdiuwahdiwa

--- js/script.js
+++ js/script.js
@@ -17,23 +17,32 @@ $(document).ready(function() {
   console.log(luca);
 });
 
-function getHolidays(monthNumber,daysInMonth) {
+function getHolidays(monthNumber, monthName, daysInMonth) {
   $.ajax(
     {
       url: "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month="+monthNumber+"",
       method: "GET",
       success: function(response) {
         console.log(response.response[0].date);
+        var source = $('#calendar_template').html();
+        var calendar = Handlebars.compile(source);
         for (var days = 1; days <= daysInMonth; days++) {
-
-        }
-        for (var i = 0; i < response.response.length; i++) {
-
-        }
+          var day = {
+            dayNumber: day+'',
+            monthName: monthName,
+            holidayName: function() {
+                for (var i = 0; i < response.response.length; i++) {
+                  if ( response.response[i].data.format('dd') == days+"" ) return response.response[i].name;
+                }
+            },
+          };
+          var html = calendar(day);
+          $('#calendar').append(html);
+        };
       },
       error: function(request, stats, errors) {
-
-      }
+        alert('Mhè');
+      },
     }
-  )
+  );
 }
