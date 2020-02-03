7b8e71bce7f9326730041e2d08899984873a2309
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-02-03 17:48:36 +0100

    passi avanti script

--- js/script.js
+++ js/script.js
@@ -12,9 +12,7 @@ $(document).ready(function() {
   var monthNumber = parseInt( moment().startOf('years').format('m') );
   var daysInMonth = parseInt( moment().startOf('years').format('D') );
   var monthName = moment().startOf('years').format('MMMM');
-  getHolidays(monthNumber);
-  var luca = moment().format('YYYY-MM-DD');
-  console.log(luca);
+  getHolidays(monthNumber, monthName, daysInMonth);
 });
 
 function getHolidays(monthNumber, monthName, daysInMonth) {
@@ -23,16 +21,25 @@ function getHolidays(monthNumber, monthName, daysInMonth) {
       url: "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month="+monthNumber+"",
       method: "GET",
       success: function(response) {
-        console.log(response.response[0].date);
+        // var paolo = response.response[0].date;
+        // var giulio = paolo[paolo.length-2]+paolo[paolo.length-1];
+        // console.log(paolo, giulio);
         var source = $('#calendar_template').html();
         var calendar = Handlebars.compile(source);
         for (var days = 1; days <= daysInMonth; days++) {
+          console.log(days,daysInMonth);
           var day = {
-            dayNumber: day+'',
+            dayNumber: days+'',
             monthName: monthName,
             holidayName: function() {
                 for (var i = 0; i < response.response.length; i++) {
-                  if ( response.response[i].data.format('dd') == days+"" ) return response.response[i].name;
+                  console.log(i);
+                  var dayNumber = response.response[i].date;
+                  dayNumber = dayNumber[dayNumber.length-2]+dayNumber[dayNumber.length-1];
+                  var thisDay = days+'';
+                  if (days<10) thisDay = '0'+thisDay;
+                  console.log(dayNumber, dayNumber == thisDay)
+                  if ( dayNumber == thisDay ) return response.response[i].name;
                 }
             },
           };
